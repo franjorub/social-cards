a341d0d4befb00986bf1b41acc6b1a4385fbb3e7
Francisco Rubial <franjorub@gmail.com>
2019-01-01 15:44:02 -0400

    Updates

---
+++ precache-manifest.60e9f15d0807a520e07f22addb889085.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e341d7bfe3c8fe931b74",
+    "url": "/social-cards/static/js/runtime~main.e341d7bf.js"
+  },
+  {
+    "revision": "a69bbc0618aa8f1a533b",
+    "url": "/social-cards/static/js/main.a69bbc06.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/social-cards/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "a69bbc0618aa8f1a533b",
+    "url": "/social-cards/static/css/main.ddbee9e2.chunk.css"
+  },
+  {
+    "revision": "a0123a478dbe7b72146aec95bcf4df71",
+    "url": "/social-cards/index.html"
+  }
+];
